cb8229c5c1790c18b86d81e9f783115c3e42c402
Manuele Menozzi <manuele.menozzi@gmail.com>
2021-08-26 08:46:16 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo chsh -s $(which zsh) $(whoami)
+
+git clone https://github.com/zdharma/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
