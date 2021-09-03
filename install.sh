b4696d4973309a1ef1aa6dd8b681f8f300251fe3
Manuele Menozzi <mmenozzi@webgriffe.com>
2021-09-03 07:58:45 +0000

    Improve install.sh and zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 sudo chsh -s $(which zsh) $(whoami)
 
 git clone https://github.com/zdharma/fast-syntax-highlighting.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt" --depth=1
+ln -s "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
+
+git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-z"
+
+
+(cd $SCRIPT_DIR && git submodule init && git submodule update)
+bash $SCRIPT_DIR/dotbot-install
